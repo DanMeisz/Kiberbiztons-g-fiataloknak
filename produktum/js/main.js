70f2c591df3338d27361a6c9cef762e33d9f89f6
MagyarBalintMate <magyarbalintmate@gmail.com>
2026-08-26 17:38:34 +0200

    Implementing js event listener scoring

--- produktum/js/main.js
+++ produktum/js/main.js
@@ -1,7 +1,21 @@
-const passwordInput = document.getElementByIs("passwordInput")
+const passwordInput = document.getElementById("passwordInput");
 
 passwordInput.addEventListener("input", function () {
     const password = passwordInput.value;
 
-    console.log(password);
+    const hasLength = password.length >= 14;
+    const hasLowercase = /[a-z]/.test(password);
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecial = /[^A-Za-z0-9]/.test(password);
+
+    let score = 0;
+
+    if (hasLength) score += 20;
+    if (hasLowercase) score += 20;
+    if (hasUppercase) score += 20;
+    if (hasNumber) score += 20;
+    if (hasSpecial) score += 20;
+
+console.log("Score:", score);
 });
